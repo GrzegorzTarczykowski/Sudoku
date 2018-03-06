d91233e2f17531897e507fe51828425e5bb6e683
Grzechu <Grzechu@GRZECHU-PC>
2018-03-06 11:17:45 +0100

    commit

---
+++ Sudoku_Tarczykowski/Sudoku_Tarczykowski/ViewModel/GameSessioncs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Sudoku_Tarczykowski.View;
+
+namespace Sudoku_Tarczykowski.ViewModel
+{
+    class GameSessioncs : INotifyPropertyChanged
+    {
+        private Page mainFrame;
+        public GameSessioncs()
+        {
+            MainFrame = new PlaygroundPage();
+            MainFrame.DataContext = this;
+        }
+        #region Property
+        public Page MainFrame
+        {
+            get { return mainFrame; }
+            set
+            {
+                if (value != mainFrame)
+                {
+                    mainFrame = value;
+                    OnPropertyChanged("MainFrame");
+                }
+            }
+        }
+        #endregion
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
